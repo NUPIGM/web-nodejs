8b40ae2db8af7081b76a4002e910817701303715
NUPIGM <53999409+NUPIGM@users.noreply.github.com>
2023-12-24 22:10:42 +0800

    g
    g

---
+++ my_modules/log.js
@@ -0,0 +1,38 @@
+import { createWriteStream, existsSync, mkdirSync } from "node:fs";
+import { format } from "node:util";
+
+const logFilePath = "log/error.log";
+// 确保日志目录存在
+if (!existsSync("log")) {
+  mkdirSync("log", { recursive: true });
+}
+// 创建写入流并设置为追加模式
+const logFileStream = createWriteStream(logFilePath, { flags: "a" });
+/**
+ * @param {object} msg  错误信息
+ * @param {string} ip   IP地址
+ * @returns {null}      无返回值
+ */
+function logError(msg, ip = "0.0.0.0") {
+  let ret = "";
+  if (!msg) {
+    return ret;
+  }
+  let date = new Date();
+  let time = date.toISOString().replace("T", " ").replace(/\..+/, "");
+  if (msg instanceof Error) {
+    ret = format(
+      "<%s> [%s]\n  stack = %s\n  data = %s\n",
+      time,
+      ip,
+      msg.stack,
+      JSON.stringify(msg.data)
+    );
+  } else {
+    ret = format("<%s> [%s]\n  %s\n", time, ip, msg);
+  }
+  logFileStream.write(ret);
+  logFileStream.close();
+}
+
+export { logError };
--- web-server.js
+++ web-server.js
@@ -1,101 +1,86 @@
-import http2 from "node:http2";
-import fs from "node:fs";
+import { createSecureServer } from "node:http2";
+import {createReadStream,readFileSync} from "node:fs";
 import path from "node:path";
 import zlib from "node:zlib";
 
 import { parseCookie, gzipOn, mimeTypes } from "./my_modules/constroller.js";
+import { logError } from "./my_modules/log.js";
 //import { login, register } from "./my_modules/admin.js";
 
 //端口
 const PORT = process.env.PORT || 443;
 //证书验证（测试证书）
 const options = {
-  key: fs.readFileSync("./ssl/key.pem"),
-  cert: fs.readFileSync("./ssl/cert.pem"),
+  key: readFileSync("./ssl/key.pem"),
+  cert: readFileSync("./ssl/cert.pem"),
 };
 
-function generateETagForFile(filePath) {
-  const stats = fs.statSync(filePath);
-  return stats.mtime.getTime().toString(); // 使用文件修改时间作为 ETag
-}
-http2
-  .createSecureServer(options, (req, res) => {
-    //url解析,返回一个对象(有路径、参数、地址等)。
-    const reqUrl = new URL(req.url, `http://${req.headers.host}`);
-    const cookies = req.headers.cookie || "";
-    const getCookies = parseCookie(cookies);
-    const ip = req.socket.remoteAddress;
-    const hv = req.httpVersion;
-    const acceptEncoding = req.headers["accept-encoding"];
+createSecureServer(options, (req, res) => {
+  //url解析,返回一个对象(有路径、参数、地址等)。
+  const reqUrl = new URL(req.url, `http://${req.headers.host}`);
+  const cookies = req.headers.cookie || "";
+  const getCookies = parseCookie(cookies);
+  const ip = req.socket.remoteAddress;
+  const hv = req.httpVersion;
+  const acceptEncoding = req.headers["accept-encoding"];
 
-    // 先判断请求的方法，决定处理过程
-    // GET || POST
-    switch (req.method) {
-      case "GET": // 请求API，不同路径执行不同内容
-        switch (reqUrl.pathname) {
-          case "/":
-            res.writeHead(301, { Location: "/documents/main.html" });
-            res.end("301!");
-            break;
+  // 先判断请求的方法，决定处理过程
+  // GET || POST
+  switch (req.method) {
+    case "GET": // 请求API，不同路径执行不同内容
+      switch (reqUrl.pathname) {
+        case "/":
+          res.writeHead(301, { Location: "/documents/main.html" });
+          res.end("301!");
+          break;
 
-          default:
-            // 使用 path 模块处理请求路径，确保只能访问 public 文件夹下的内容
-            const filePath = path.join(
-              process.cwd(),
-              "public",
-              reqUrl.pathname
-            );
-            //读取文件流
-            const readStream = fs.createReadStream(filePath);
+        default:
+          // 使用 path 模块处理请求路径，确保只能访问 public 文件夹下的内容
+          const filePath = path.join(process.cwd(), "public", reqUrl.pathname);
+          //读取文件流
+          const readStream = createReadStream(filePath);
 
-            //访问文件的MIME类型
-            const fileExt = path.extname(filePath);
-            const mime = mimeTypes[fileExt];
-            const etag = generateETagForFile(filePath); // 生成唯一的 ETag 值
+          //访问文件的MIME类型
+          const fileExt = path.extname(filePath);
+          const mime = mimeTypes[fileExt];
 
-            readStream.on("data", (chunk) => {}); //OK
-            readStream.on("error", (err) => {
-              const etag = generateETagForFile(); // 生成唯一的 ETag 值
-              const lastModified = getLastModifiedTimeForFile();
-              res.writeHead(405, {
-                "Content-Type": "text/html",
-                "Content-Encoding": "br", // 设置 Brotli 压缩});
-                ETag: etag,
-                "Cache-Control:public": "max-age=3600",
-                "Last-Modified": lastModified.toUTCString(),
-              });
+          readStream.on("data", (chunk) => {});
+          readStream.on("end", () => {});
+          readStream.on("error", (err) => {
+            res.writeHead(404, {});
+            res.end();
+            // console.error("在try/catch块中捕获的错误:", err);
+            logError(err, ip);
+          });
+          readStream.pipe(res);
 
-              res.end("Method Not Allowed");
-              console.log(err);
-            });
-            if (mime) {
-              res.setHeader("Content-Type", mime + ";charset=utf-8");
-            }
-            const ifNoneMatch = req.headers["if-none-match"];
-            if (ifNoneMatch === etag) {
-              res.writeHead(304, {
-                ETag: etag,
-              });
-              res.end(); // 资源未改变，返回 304 Not Modified
-            } else if (acceptEncoding.includes("br")) {
-              res.writeHead(200, {
-                "Content-Encoding": "br", // 设置 Brotli 压缩});
-                ETag: etag,
-                "Cache-Control": "public,max-age=1",
-              });
-              readStream.pipe(zlib.createBrotliCompress()).pipe(res);
-            } else if (acceptEncoding.includes("gzip")) {
-              res.setHeader("Content-Encoding", "gzip");
-              readStream.pipe(zlib.createGzip()).pipe(res);
-            } else {
-              readStream.pipe(res);
-            }
-            readStream.on("end", () => {});
-            break;
-        }
+          // if (mime) {
+          //   res.setHeader("Content-Type", mime + ";charset=utf-8");
+          // }
+          // const ifNoneMatch = req.headers["if-none-match"];
+          // if (ifNoneMatch === etag) {
+          //   res.writeHead(304, {
+          //     ETag: etag,
+          //   });
+          //   res.end(); // 资源未改变，返回 304 Not Modified
+          // } else if (acceptEncoding.includes("br")) {
+          //   res.writeHead(200, {
+          //     "Content-Encoding": "br", // 设置 Brotli 压缩});
+          //     ETag: etag,
+          //     "Cache-Control": "public,max-age=1",
+          //   });
+          //   readStream.pipe(zlib.createBrotliCompress()).pipe(res);
+          // } else if (acceptEncoding.includes("gzip")) {
+          //   res.setHeader("Content-Encoding", "gzip");
+          //   readStream.pipe(zlib.createGzip()).pipe(res);
+          // } else {
+          //   readStream.pipe(res);
+          // }
+          break;
+      }
 
-        break;
-      /*
+      break;
+    /*
         case "POST": // 请求API，不同路径执行不同内容
         switch (reqUrl.pathname) {
           case "/admin":
@@ -152,8 +137,7 @@ http2
         }
         break;
       */
-    }
-  })
-  .listen(PORT, () => {
-    console.log("web started 127.0.0.1:", PORT);
-  });
+  }
+}).listen(PORT, () => {
+  console.log("web started https://127.0.0.1:", PORT);
+});
